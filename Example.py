6c86e7dfc5f7d0724ae22ebf9f131a1b9cf2b023
isela13 <43421812+isela13@users.noreply.github.com>
2025-10-30 19:52:03 -0600

    Refactor requests code in Example.py

--- Example.py
+++ Example.py
@@ -1,12 +1,7 @@
 
-# import requests module
+
 import requests
-
-# Making a get request
 response = requests.get('https://wmxrwq14uc.execute-api.us-east-1.amazonaws.com/Prod/Account/LogIn')
-
-# print request object
 print(response.url)
 
-# print status code
 print(response.status_code)
